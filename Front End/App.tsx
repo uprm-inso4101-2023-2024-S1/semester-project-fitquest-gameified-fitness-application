b9ae4971b19dbbfac29f3a03174be5856ae726ae
JConcepcion01 <jomard.concepcion@upr.edu>
2023-09-15 23:22:31 -0400

    Added Multiple Page Navigation

--- Front End/App.tsx
+++ Front End/App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StopWatch from './components/StopWatch';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { workoutPage } from './pages/WorkoutPage';
+import { socialPage } from './pages/SocialPage';
+import { profilePage } from './pages/ProfilePage';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StopWatch/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Workout" component={workoutPage} />
+        <Tab.Screen name="Social" component={socialPage} />
+        <Tab.Screen name="Profile" component={profilePage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
