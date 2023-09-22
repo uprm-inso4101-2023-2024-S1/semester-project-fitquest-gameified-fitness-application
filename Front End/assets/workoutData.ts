15484765b9f44485ea7524b4983e3bd9d5896712
oscaroflores <oscar.flores@upr.edu>
2023-09-22 14:55:14 -0400

    Added temporary workout dummy data

--- Front End/assets/workoutData.ts
+++ Front End/assets/workoutData.ts
@@ -3,27 +3,30 @@
 export let Workouts = [
   {
     key: 0,
-    workout_name: "",
+    workout_name: "Full Body from Home",
     exercises: [
-      { name: "", duration: 10000, description: "", gif_link: "" },
-      { name: "", duration: 10000, description: "", gif_link: "" },
+      { name: "Squats", duration: 10000, description: "", gif_link: "" },
+      { name: "Push Ups", duration: 10000, description: "", gif_link: "" },
+      { name: "Pull Ups", duration: 10000, description: "", gif_link: "" },
     ], //More field may need to be added as needed.
   },
   ,
   {
     key: 1,
-    workout_name: "",
+    workout_name: "Arms",
     exercises: [
-      { name: "", duration: 10000, description: "", gif_link: "" },
-      { name: "", duration: 10000, description: "", gif_link: "" },
+      { name: "Single Arm Row", duration: 10000, description: "", gif_link: "" },
+      { name: "Overhead Shoulder Press", duration: 10000, description: "", gif_link: "" },
+      { name: "Reverse Fly", duration: 10000, description: "", gif_link: "" },
     ], //More field may need to be added as needed.
   },
   {
     key: 2,
-    workout_name: "",
+    workout_name: "Legs",
     exercises: [
-      { name: "", duration: 10000, description: "", gif_link: "" },
-      { name: "", duration: 10000, description: "", gif_link: "" },
+      { name: "Squats", duration: 10000, description: "", gif_link: "" },
+      { name: "Walking Lunges", duration: 10000, description: "", gif_link: "" },
+      { name: "Step Ups", duration: 10000, description: "", gif_link: "" },
     ], //More field may need to be added as needed.
   },
 ]; //Todo Add workout dummy data
