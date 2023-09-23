3d007ae75e1ad5c99ca6253e1caf695c7b73b4e0
IvanierB <ivanier.bellido@upr.edu>
2023-09-23 14:51:28 -0400

    Display premade workouts

--- Front End/pages/WorkoutPage.tsx
+++ Front End/pages/WorkoutPage.tsx
@@ -1,17 +1,42 @@
-import { View, Text, Button } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { Workouts } from "../assets/workoutData";
 
 export default function WorkoutPage({ navigation }) {
+  const [selectedWorkout, setSelectedWorkout] = useState(null);
+
+  const selectWorkout = (workout) => {
+    setSelectedWorkout(workout === selectedWorkout ? null : workout);
+  };
+
   return (
-    <View>
-      <Text>Menu of Woorkouts and Button to custom Workout screen</Text>
-      <Button
-        title="Create Your Own"
-        onPress={() => navigation.navigate("CustomWorkout")}
-      />
-      <Text>
-        Premade Workouts, When one of them is selected the Timer has to queue
-        and start all the intervals of the exercices
-      </Text>
+    <View style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Workout Page</Text>
+
+      {/* Display the list of workouts using map */}
+      {Workouts.map((item) => {
+        const isSelected = item === selectedWorkout;
+        return (
+          <TouchableOpacity key={item.key} onPress={() => selectWorkout(item)}>
+            <View style={{ padding: 16, backgroundColor: isSelected ? 'lightblue' : 'white', marginBottom: 8 }}>
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.workout_name}</Text>
+              {isSelected && (
+                <View>
+                  <Text style={{ fontSize: 18, marginTop: 8 }}>Exercises:</Text>
+                  {item.exercises.map((exercise, index) => (
+                    <View key={index} style={{ marginLeft: 16 }}>
+                      <Text>Exercise {index + 1}: {exercise.name}</Text>
+                      <Text>Duration: {exercise.duration} ms</Text>
+                      <Text>Description: {exercise.description}</Text>
+                      <Text>GIF Link: {exercise.gif_link}</Text>
+                    </View>
+                  ))}
+                </View>
+              )}
+            </View>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 }
