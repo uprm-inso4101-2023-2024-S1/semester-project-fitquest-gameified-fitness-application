39cae118560a7ee10953a42d7515889c4124d195
edgarsuarez123 <edgar.suarez1@upr.edu>
2023-09-22 22:55:25 -0400

    implemented exercises data in assets folder

---
+++ Front End/assets/exercisesData.ts
@@ -0,0 +1,18 @@
+const exercises = [
+    { name: "Overhead Press", category: "arms", info: "A compound exercise that targets the shoulders and triceps while also engaging the core." },
+    { name: "Bicep Curl", category: "arms", info: "An isolation exercise for the biceps, usually performed with dumbbells." },
+    { name: "Tricep Dip", category: "arms", info: "Targets the triceps and can be done using parallel bars or a sturdy surface." },
+  
+    { name: "Squat", category: "legs", info: "A compound lower body exercise that targets the quadriceps, hamstrings, and glutes." },
+    { name: "Lunges", category: "legs", info: "A unilateral leg exercise that strengthens the quadriceps and glutes." },
+    { name: "Deadlift", category: "legs", info: "A powerful compound exercise that targets the lower back, hamstrings, and glutes." },
+  
+    { name: "Pull-Up", category: "back", info: "A bodyweight exercise that targets the back, specifically the lats." },
+    { name: "Rowing", category: "back", info: "A machine or barbell exercise that works the upper back and traps." },
+    { name: "Lat Pulldown", category: "back", info: "An exercise that targets the latissimus dorsi muscles using a cable machine." },
+  
+    { name: "Crunches", category: "abs", info: "An abdominal exercise that helps strengthen the rectus abdominis." },
+    { name: "Plank", category: "abs", info: "A core-strengthening exercise that engages the entire abdominal region." },
+    { name: "Russian Twists", category: "abs", info: "An exercise that targets the obliques and core muscles." },
+  ];
+  
